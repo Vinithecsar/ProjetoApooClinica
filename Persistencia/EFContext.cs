ce7eb67bf315af81227fc0ea8418b83f2b313928
Vinícius César <v.cesar@academico.ifrn.edu.br>
2022-10-26 16:23:42 -0300

    Adicionar arquivos de projeto.

---
+++ Persistencia/EFContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Modelo;
+
+namespace Persistencia
+{
+    public class EFContext : DbContext
+    {
+        public EFContext() : base("Asp_Net_MVC_CS")
+        {
+            Database.SetInitializer<EFContext>(
+            new DropCreateDatabaseIfModelChanges<EFContext>());
+        }
+        public DbSet<Exame> Exames { get; set; }
+    }
+}
