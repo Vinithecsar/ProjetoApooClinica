f19cab05095407ddd5b5e8448c56acbe82ef85b2
20201011110002 <20201011110002@CNAT407144A>
2022-11-11 11:42:26 -0300

    Classe ConsultaExame

---
+++ Modelo/ConsultaExame.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+    public class ConsultaExame
+    {
+        public int ConsultaExameId { get; set; }
+        public int ConsultaId { get; set; }
+        public int ExameId { get; set; }
+        public virtual Consulta Consulta { get; set; }
+        public virtual Exame Exame { get; set; }
+    }
+}
